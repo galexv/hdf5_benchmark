b0392f9ce41b9709e695ebaab9572a4b999c2b72
Alexander Gaenko <galexv@umich.edu>
2020-02-17 22:31:22 -0500

    Added a small library for command line parsing

---
+++ dependencies/cmdline/cmdline.hpp
@@ -0,0 +1,110 @@
+/** @file Simple command line argument parsing library */
+
+#include <string>
+#include <sstream>
+#include <map>
+
+namespace program_options {
+
+    template <typename T>
+    class optional {
+        T val_;
+        bool valid_;
+        
+        public:
+        explicit optional(const T& val): val_(val), valid_(true) {}
+        optional(): val_(), valid_(false) {}
+        
+        explicit operator bool() const
+        {
+            return valid_;
+        }
+        
+        const T& operator*() const
+        {
+            if (!valid_) throw std::runtime_error("dereferencing an empty optional");
+            return val_;
+        }
+    };
+
+    namespace detail {
+        template <typename T>
+        optional<T> make_optional(const T& val)
+        {
+            return optional<T>(val);
+        }
+
+        template <typename T>
+        optional<T> try_lexical_cast(const std::string& s) {
+            std::istringstream strm(s);
+            noskipws(strm);
+            T val;
+            strm >> val;
+            if (strm && strm.eof()) return make_optional(val);
+            return optional<T>();
+        }
+
+        template <>
+        optional<bool> try_lexical_cast(const std::string& s) {
+            for (const auto* cval: {"false","FALSE","off","OFF","no","NO","0"}) {
+                if (cval==s) return detail::make_optional(false);
+            }
+            for (const auto* cval: {"true","TRUE","on","ON","yes","YES","1"}) {
+                if (cval==s) return detail::make_optional(true);
+            }
+            return optional<bool>();
+        }
+        
+    }
+    
+    
+    class params_map {
+        using map_type = std::map<std::string,std::string>;
+        map_type map_;
+        
+      public:
+        // using parsed_type=optional<params_map>;
+        
+        template <typename T>
+        optional<T> get(const std::string& key) const
+        {
+            auto it = map_.find(key);
+            if (it == map_.end()) return optional<T>();
+            auto maybe_val = detail::try_lexical_cast<T>(it->second);
+            return maybe_val;
+        }
+
+        template <typename T>
+        optional<T> get_or(const std::string& key, const T& deflt) const
+        {
+            auto it = map_.find(key);
+            if (it == map_.end()) return detail::make_optional(deflt);
+            auto maybe_val = detail::try_lexical_cast<T>(it->second);
+            return maybe_val;
+        }
+
+        optional<std::string> get_or(const std::string& key, const char* deflt) const
+        {
+            return get_or(key, std::string(deflt));
+        }
+
+        friend optional<params_map> parse(int argc, const char* const* argv);
+    };
+
+    
+    inline optional<params_map> parse(int argc, const char* const* argv) {
+        using std::string;
+        using return_type = optional<params_map>;
+        params_map p;
+        for (int i=1; i<argc; ++i) {
+            std::string arg{argv[i]};
+            auto eq_pos=arg.find('=');
+            if (eq_pos==string::npos || eq_pos<1) {
+                return return_type();
+            }
+            p.map_[arg.substr(0, eq_pos)] = arg.substr(eq_pos+1);
+        }
+        return return_type(p);
+    }
+
+}
---
+++ dependencies/cmdline/cmdline_test.cpp
@@ -0,0 +1,127 @@
+#include "./cmdline.hpp"
+#include <gtest/gtest.h>
+
+#include <vector>
+
+namespace po=program_options;
+
+struct program_options_Test: public ::testing::Test {
+    using string=std::string;
+    po::params_map params;
+    
+    program_options_Test() {
+        static const char* argv[] = {"program.x", "a_number=1111", "an_option=false", "a_string=hello"};
+        const int argc = sizeof(argv)/sizeof(*argv);
+        auto maybe_params = po::parse(argc, argv);
+        params = *maybe_params;
+    }
+    
+    
+};
+
+TEST_F(program_options_Test, wrongParams) {
+    static const char* argv[] = {"program.x", "something", "a_number=1111",
+                                 "an_option=false", "a_string=hello"};
+    const int argc = sizeof(argv)/sizeof(*argv);
+    auto maybe_params = po::parse(argc, argv);
+    EXPECT_FALSE(maybe_params);
+}
+
+
+TEST_F(program_options_Test, intNumber) {
+    auto maybe_number=params.get<int>("a_number");
+    ASSERT_TRUE(bool(maybe_number));
+    EXPECT_EQ(1111, *maybe_number);
+}
+
+
+TEST_F(program_options_Test, intNumberDefault) {
+    auto maybe_number=params.get_or("a_number",7777);
+    ASSERT_TRUE(bool(maybe_number));
+    EXPECT_EQ(1111, *maybe_number);
+}
+
+
+TEST_F(program_options_Test, intNumberAsString) {
+    auto maybe_string=params.get<string>("a_number");
+    ASSERT_TRUE(bool(maybe_string));
+    EXPECT_EQ("1111", *maybe_string);
+}
+
+
+TEST_F(program_options_Test, intNumberAsStringDefault) {
+    auto maybe_string=params.get_or("a_number", "7777");
+    ASSERT_TRUE(bool(maybe_string));
+    EXPECT_EQ("1111", *maybe_string);
+}
+
+
+TEST_F(program_options_Test, stringParam) {
+    auto maybe_string=params.get<string>("a_string");
+    ASSERT_TRUE(bool(maybe_string));
+    EXPECT_EQ("hello", *maybe_string);
+}
+
+
+TEST_F(program_options_Test, stringParamDefault) {
+    auto maybe_string=params.get_or("a_string", "bye");
+    ASSERT_TRUE(bool(maybe_string));
+    EXPECT_EQ("hello", *maybe_string);
+}
+
+
+TEST_F(program_options_Test, stringAsNumber) {
+    auto maybe_number=params.get<int>("a_string");
+    EXPECT_FALSE(bool(maybe_number));
+    EXPECT_ANY_THROW(*maybe_number);
+}
+
+
+TEST_F(program_options_Test, stringAsNumberDefault) {
+    auto maybe_number=params.get_or("a_string", 7777);
+    EXPECT_FALSE(bool(maybe_number));
+    EXPECT_ANY_THROW(*maybe_number);
+}
+
+
+TEST_F(program_options_Test, missingIntNumber) {
+    auto maybe_number=params.get<int>("missing");
+    EXPECT_FALSE(bool(maybe_number));
+    EXPECT_ANY_THROW(*maybe_number);
+}
+
+
+TEST_F(program_options_Test, missingIntNumberDefault) {
+    auto maybe_number=params.get_or("missing",7777);
+    ASSERT_TRUE(bool(maybe_number));
+    EXPECT_EQ(7777, *maybe_number);
+}
+
+
+TEST_F(program_options_Test, missingStringDefault) {
+    auto maybe_string=params.get_or("missing","bye");
+    ASSERT_TRUE(bool(maybe_string));
+    EXPECT_EQ("bye", *maybe_string);
+}
+
+
+TEST_F(program_options_Test, boolParam) {
+    auto maybe_option=params.get<bool>("an_option");
+    ASSERT_TRUE(bool(maybe_option));
+    EXPECT_FALSE(*maybe_option);
+}
+
+
+TEST_F(program_options_Test, boolParamDefault) {
+    auto maybe_option=params.get_or("an_option", true);
+    ASSERT_TRUE(bool(maybe_option));
+    EXPECT_FALSE(*maybe_option);
+}
+
+
+TEST_F(program_options_Test, missingBoolDefault) {
+    auto maybe_option=params.get_or("missing", false);
+    ASSERT_TRUE(bool(maybe_option));
+    EXPECT_FALSE(*maybe_option);
+}
+
